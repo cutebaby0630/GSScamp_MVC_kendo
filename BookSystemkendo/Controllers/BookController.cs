13351a2653e422ebae8caf3a2ac99dada3377484
cutebaby0630 <lovemath0630@gmail.com>
2019-07-05 17:17:19 +0800

    DropDownList

---
+++ BookSystemkendo/Controllers/BookController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Web.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BookSystemkendo.Controllers
+{
+    public class BookController : Controller
+    {
+        readonly Models.CodeService codeService = new Models.CodeService();
+        
+        // GET: Book
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public JsonResult GetClassNameDropDownList()
+        {
+            return Json(this.codeService.GetBookClassName());
+        }
+        [HttpPost]
+        public JsonResult GetUserNameDropDownList()
+        {
+            return Json(this.codeService.GetUserName());
+        }
+        [HttpPost]
+        public JsonResult GetCodeNameDropDownList()
+        {
+            return Json(this.codeService.GetCodeName());
+        }
+    }
+}
---
+++ BookSystemkendo/Models/BookSearchArg.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel;
+
+namespace BookSystemkendo.Models
+{
+    public class BookSearchArg
+    {
+
+        [DisplayName("書名")]
+        public string BookName { get; set; }
+        [DisplayName("借閱人")]
+        public string UserName { get; set; }
+        [DisplayName("圖書類別")]
+        public string BookClassName { get; set; }
+        [DisplayName("借閱狀態")]
+        public string CodeName { get; set; }
+        [DisplayName("借閱狀態ID")]
+        public string CodeID { get; set; }
+    }
+}
